d778687fb8f42539b61444fafc1606810c06b460
Gangrade@8 <ankitgangrade9617@gmail.com>
2025-08-13 23:22:09 +0530

    backend url ubdate

--- backend/server.js
+++ backend/server.js
@@ -31,9 +31,11 @@ dotenv.config();
 
 const app = express();
 const server = createServer(app);
+const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:3000';
+
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [`${FRONTEND_URL}`, "http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
   }
 });
